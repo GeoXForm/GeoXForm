b6134eb806a8e8fe3cd9e0171ad2499cf931ae13
dmfenton <dfenton@esri.com>
2015-12-20 15:18:57 -0500

    linting

--- src/lib/geojson.js
+++ src/lib/geojson.js
@@ -4,10 +4,10 @@ function createStream (options) {
   const start = '{"type":"FeatureCollection","features":['
   const end = ']}'
   const readStream = _.pipeline(s => {
-   const features = options && options.json ? _(s).compact().map(JSON.stringify) : _(s).compact()
-   return  _([start])
-   .concat(features.intersperse(','))
-   .append(end)
+    const features = options && options.json ? _(s).compact().map(JSON.stringify) : _(s).compact()
+    return _([start])
+      .concat(features.intersperse(','))
+      .append(end)
   })
   return readStream
 }
