b960a7f6ab9c8466d32345eb634e93f772a893a6
dmfenton <dfenton@esri.com>
2016-04-13 11:38:15 -0400

    respect 2GB shapefile dbf limit

--- src/lib/ogr-cmd.js
+++ src/lib/ogr-cmd.js
@@ -48,6 +48,7 @@ function shapefileParams (cmd, options) {
   // make sure geometries are still written even if the first is null
   cmd.push('-nlt', options.geometry.toUpperCase())
   cmd.push('-fieldmap', 'identity')
+  if (!options.ignoreShpLimit) cmd.push('-lco', '2GB_LIMIT=yes')
   if (options.srs) cmd.push('-t_srs', options.srs)
   return cmd
 }
